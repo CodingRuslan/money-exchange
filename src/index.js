5b80511fd89dffa177843a2aceab82239ff0f748
CodingRuslan <orion.hola@gmail.com>
2019-03-09 10:42:39 +0300

    feat: implement function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let d = {0:50, 1:25, 2:10, 3:5, 4:1};
+    
+    let res = {};//makeExchange(9999), {"H":199,"Q":1,"D":2,"P":4}
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    } else {
+        for (let i = 0; i < 5; i++) {
+            let count = 0;
+            if (currency/d[i] >= 1 && i === 0) {
+                count =  Math.floor(currency/d[i]);
+                currency -= count * d[i];
+                res["H"] = count;
+            } else if(currency/d[i] >= 1 && i === 1){
+                count = Math.floor(currency/d[i]);
+                currency -= count * d[i];
+                res["Q"] = count;
+            } else if (currency/d[i] >= 1 && i === 2) {
+                count = Math.floor(currency/d[i]);
+                currency -= count * d[i];
+                res["D"] = count;
+            } else if (currency/d[i] >= 1 && i === 3) {
+                count = Math.floor(currency/d[i]);
+                currency -= count * d[i];
+                res["N"] = count;
+            } else if (currency/d[i] >= 1 && i === 4) {
+                count = Math.floor(currency/d[i]);
+                currency -= count * d[i];
+                res["P"] = count;
+            }
+        }return res;
+    }
+    
 }
